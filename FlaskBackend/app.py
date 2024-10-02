f8bd83ebc880a8220bcb3fae7b875fccfc5cd536
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-10-02 17:12:37 -0400

    adding basic user data range api

--- FlaskBackend/app.py
+++ FlaskBackend/app.py
@@ -68,9 +68,38 @@ def get_data(username):
 
     return jsonify({'username': username, 'data': data}), 200
 
+@app.route('/data/range/<username>', methods=['GET'])
+def get_data_range(username):
+    """
+    The purpose of this func is to get the data of a user within a certain range of time specified by the user. The user will specify the start and end time of the range and the function will return the data within that range.
+
+    Args:
+        username (_type_): The username of the user whose data is to be fetched.
+    """
+    user = User.query.filter_by(username=username).first()
+    
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+    
+    start_time = request.args.get('start_time')
+    end_time = request.args.get('end_time')
+    
+    if not all([start_time, end_time]):
+        return jsonify({'error': 'Missing start_time or end_time'}), 400
+    
+    start_time = datetime.fromisoformat(start_time)
+    end_time = datetime.fromisoformat(end_time)
+    
+    data = [{
+        'angle': d.angle,
+        'rotation': d.rotation,
+        'timestamp': d.timestamp.isoformat()
+    } for d in user.device_data if start_time <= d.timestamp <= end_time]
+    
+    return jsonify({'username': username, 'data': data}), 200
+
 def main():
     app.run(debug=True)
 
 if __name__ == '__main__':
     main()
-
